15f9ad2dcb7e11263213d50eec8a3b81cb6cbcb4
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-21 13:02:31 +0530

    tun: linux support

--- src/tun/linux.rs
+++ src/tun/linux.rs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, Krisna Pranav
+ *
+ * SPDX-License-Identifier: MIT License
+ */
+
 use std::io;
 use std::io::{Read, Write};
 use std::io::Result;
@@ -11,4 +17,65 @@ use crate::tun::{Rx, TunDevice, Tx};
 
 pub struct Linuxtun {
     fd: Device,
+}
+
+impl Linuxtun {
+    pub fn create(address: Ipv4Addr, netmask: Ipv4Addr) -> Result<Linuxtun> {
+        let mut config = tun::Configuration::default();
+        config.address(address)
+            .netmask(netmask)
+            .mtu(MTU as i32)
+            .up();
+
+        config.platform(|config| {
+            config.packet_information(false);
+        });
+
+        Ok(Linuxtun {
+            fd: tun::create(&config).map_err(|e| {
+                io::Error::new(io::ErrorKind::Other, e)
+            })?
+        })
+    }
+}
+
+struct LinuxtunTx {
+    tx: Writer,
+}
+
+struct LinuxtunRx {
+    rx: Reader,
+}
+
+impl Tx for LinuxtunTx {
+    fn send_packet(&mut self, packet: &[u8]) -> Result<()> {
+        self.tx.write(packet)?;
+        Ok(())
+    }
+}
+
+impl Rx for LinuxtunRx {
+    fn recv_packet(&mut self, buff: &mut [u8]) -> Result<usize> {
+        self.rx.read(buff)
+    }
+}
+
+impl Rx for Linuxtun {
+    fn recv_packet(&mut self, buff: &mut [u8]) -> Result<usize> {
+        self.fd.read(buff)
+    }
+}
+
+impl Tx for Linuxtun {
+    fn send_packet(&mut self, packet: &[u8]) -> Result<()> {
+        self.fd.write(packet)?;
+        Ok(())
+    }
+}
+
+impl TunDevice for Linuxtun {
+    fn split(self) -> (Box<dyn Tx>, Box<dyn Rx>) {
+        let (rx, tx) = self.fd.split();
+        (Box::new(LinuxtunTx { tx }), Box::new(LinuxtunRx { rx }))
+    }
 }
